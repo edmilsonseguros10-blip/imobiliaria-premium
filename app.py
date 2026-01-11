8db4df8da1e70e2390ab30037201420f977ccd43
edmilsonseguros10-blip <edmilsonseguros10@gmail.com>
2026-01-11 12:14:55 -0400

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,6 @@ from PIL import Image
 import io
 # --- FUNÇÃO DE CONEXÃO (MODO NUVEM NEON ☁️) ---
 def get_db_connection():
-    # ⚠️ IMPORTANTE: Apague o texto abaixo e cole o link que você copiou do Neon!
     url_do_banco = "postgres://neondb_owner:npg_HxAsIhy6q8n@ep-delicate-mud-ah3mkiw5-pooler.us-east-1.aws.neon.tech/neondb?sslmode=require"
     return psycopg2.connect(url_do_banco)
     
@@ -378,6 +377,7 @@ if menu == "🔐 Área do Corretor":
             else:
 
                 st.warning("Nenhum imóvel encontrado com esse perfil.")
+
 
 
 
